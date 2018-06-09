effbc8a2c44dd6400ed6a92d8006cacf5b4ef87e
wjx <2798075426@qq.com>
2018-06-09 16:42:47 +0800

    :wq

---
+++ app/src/main/java/com/example/adminstator/coolweather/db/City.java
@@ -0,0 +1,49 @@
+package com.example.adminstator.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Adminstator on 2018/6/7.
+ */
+
+public class City extends DataSupport {
+    private  int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    public int getId(){
+        return id;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getCityName(String cityName){
+        return cityName;
+    }
+
+    public void setCityName(String cityName){
+        this.cityName = cityName;
+    }
+
+    public int getCityCode(){
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode){
+        this.cityCode = cityCode;
+    }
+    public int getProvinceId(){
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId){
+        this.provinceId = provinceId;
+    }
+
+}
